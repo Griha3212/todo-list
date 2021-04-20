bf91030af3389eb116b4f80522f4fdc84736d514
Griha3212 <Griha3212@yandex.ru>
2021-04-20 11:02:23 +0300

    add basic redux structure

--- backend/src/models/users.js
+++ backend/src/models/users.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const usersSchema = new Schema({
-  firstName: { type: "String", required: true },
-  lastName: { type: "String", required: true },
+const userSchema = new Schema({
+  firstName: { type: "String", required: true, index: true },
+  lastName: { type: "String", required: true, index: true },
 
   //   title: { type: "String", required: true },
   //   content: { type: "String", required: true },
   //   slug: { type: "String", required: true },
   //   cuid: { type: "String", required: true },
-  dateAdded: { type: "Date", default: Date.now, required: true },
+  dateAdded: { type: "Date", default: Date.now, required: true, index: true },
 });
 
-export default mongoose.model("Users", usersSchema);
+export default mongoose.model("Users", userSchema);
